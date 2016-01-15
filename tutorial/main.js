e547a78abdf798c46a23f75c6dd13953c2346b96
Kristoffer <kristoffer.mysen@gmail.com>
2016-01-15 23:15:28 +0100

    dag to

--- tutorial/main.js
+++ tutorial/main.js
@@ -1,11 +1,18 @@
 var http  = require("http");
 
+var fs = require("fs");
+fs.readFile('input.txt', function(err, data) {
+
+	if (err) return console.error(err);
+	console.log(data.toString());
+
+});
 
 http.createServer(function (request, response) {
 
 	response.writeHead(200, {'Content-Type' : 'text/plain'})
 
-	response.end('Hello World\n');
+	response.end(data + '\n');
 
 }).listen(8081);
 
